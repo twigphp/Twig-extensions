c69e73ab33cb24e61e79ab218f0e10088752d853
Fabien Pennequin <fabien@pennequin.me>
2010-11-25 16:35:26 +0100

    Fixed test bootstraping

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -9,5 +9,22 @@
  * file that was distributed with this source code.
  */
 
-require_once dirname(__FILE__).'/../lib/Twig/Autoloader.php';
+if (!defined('TWIG_LIBRARY_PATH')) {
+    die('path to twig library must be defined in phpunit.xml configuration');
+}
+
+require_once TWIG_LIBRARY_PATH.'/Twig/Autoloader.php';
 Twig_Autoloader::register();
+
+function twigExtensionAutoload($class)
+{
+    if (0 !== strpos($class, 'Twig_Extensions')) {
+        return;
+    }
+
+    if (file_exists($file = dirname(__FILE__).'/../lib/'.str_replace('_', '/', $class).'.php')) {
+        require $file;
+    }
+}
+
+spl_autoload_register('twigExtensionAutoload');
