7597f523dabe218a7591d95a13a2a86483899936
Francisco Lopes <chico.lopes@gmail.com>
2011-04-02 13:04:00 -0300

    fixing indentation

--- lib/Twig/Extensions/Extension/Number.php
+++ lib/Twig/Extensions/Extension/Number.php
@@ -12,28 +12,28 @@
  */
 class Twig_Extensions_Extension_Number extends Twig_Extension
 {
-	/**
-	 * Returns a list of filters.
-	 *
-	 * @return array
-	 */
-	public function getFilters()
-	{
-		return array('number' => new Twig_Filter_Function('twig_number_filter'));
-	}
+    /**
+     * Returns a list of filters.
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array('number' => new Twig_Filter_Function('twig_number_filter'));
+    }
 
-	/**
-	 * Name of this extension
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'Number';
-	}
+    /**
+     * Name of this extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Number';
+    }
 }
 
 function twig_number_filter($number, $decimals = 0, $dec_point = '.', $thousands_sep = ',')
 {
-	return number_format($number, $decimals, $dec_point, $thousands_sep);
+    return number_format($number, $decimals, $dec_point, $thousands_sep);
 }
