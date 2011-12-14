6c8cf098288940d661db7969c7c02ed4261e5511
Emanuele Gaspari <inmarelibero@gmail.com>
2011-12-14 22:21:50 +0100

    typo

--- lib/Twig/Extensions/Extension/Array.php
+++ lib/Twig/Extensions/Extension/Array.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Emanuele Gaspari Castelletti <inmareliberogmail.com>
+ * @author Emanuele Gaspari Castelletti <inmarelibero@gmail.com>
  * @package Twig
  * @subpackage Twig-extensions
  */
