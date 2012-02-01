a3596896838f3626234174578165c65f33615320
Stanislav Petrov <s.e.petrov@gmail.com>
2012-02-01 17:53:12 +0000

    fixed coding style issues

--- lib/Twig/Extensions/Extension/Date.php
+++ lib/Twig/Extensions/Extension/Date.php
@@ -63,15 +63,15 @@ function twig_distance_of_time_in_words_filter(Twig_Environment $env, $from_time
 {
     if ($from_time instanceof DateTime) {
         $from_time = $from_time->getTimestamp();
-    } else if (!is_numeric($from_time)) {
+    } elseif (!is_numeric($from_time)) {
         $from_time = strtotime($from_time);
     }
 
     if ($to_time instanceof DateTime) {
         $to_time = $to_time->getTimestamp();
-    } else if (is_null($to_time)) {
+    } elseif (is_null($to_time)) {
         $to_time = time();
-    } else if (!is_numeric($to_time)) {
+    } elseif (!is_numeric($to_time)) {
         $to_time = strtotime($to_time);
     }
 
@@ -87,37 +87,37 @@ function twig_distance_of_time_in_words_filter(Twig_Environment $env, $from_time
         } else {
             if ($distance_in_seconds <= 5) {
                 $string = 'less than 5 seconds';
-            } else if ($distance_in_seconds >= 6 && $distance_in_seconds <= 10) {
+            } elseif ($distance_in_seconds >= 6 && $distance_in_seconds <= 10) {
                 $string = 'less than 10 seconds';
-            } else if ($distance_in_seconds >= 11 && $distance_in_seconds <= 20) {
+            } elseif ($distance_in_seconds >= 11 && $distance_in_seconds <= 20) {
                 $string = 'less than 20 seconds';
-            } else if ($distance_in_seconds >= 21 && $distance_in_seconds <= 40) {
+            } elseif ($distance_in_seconds >= 21 && $distance_in_seconds <= 40) {
                 $string = 'half a minute';
-            } else if ($distance_in_seconds >= 41 && $distance_in_seconds <= 59) {
+            } elseif ($distance_in_seconds >= 41 && $distance_in_seconds <= 59) {
                 $string = 'less than a minute';
             } else {
                 $string = '1 minute';
             }
         }
-    } else if ($distance_in_minutes >= 2 && $distance_in_minutes <= 44) {
+    } elseif ($distance_in_minutes >= 2 && $distance_in_minutes <= 44) {
         $string = '%minutes% minutes';
         $parameters['%minutes%'] = $distance_in_minutes;
-    } else if ($distance_in_minutes >= 45 && $distance_in_minutes <= 89) {
+    } elseif ($distance_in_minutes >= 45 && $distance_in_minutes <= 89) {
         $string = 'about 1 hour';
-    } else if ($distance_in_minutes >= 90 && $distance_in_minutes <= 1439) {
+    } elseif ($distance_in_minutes >= 90 && $distance_in_minutes <= 1439) {
         $string = 'about %hours% hours';
         $parameters['%hours%'] = round($distance_in_minutes / 60);
-    } else if ($distance_in_minutes >= 1440 && $distance_in_minutes <= 2879) {
+    } elseif ($distance_in_minutes >= 1440 && $distance_in_minutes <= 2879) {
         $string = '1 day';
-    } else if ($distance_in_minutes >= 2880 && $distance_in_minutes <= 43199) {
+    } elseif ($distance_in_minutes >= 2880 && $distance_in_minutes <= 43199) {
         $string = '%days% days';
         $parameters['%days%'] = round($distance_in_minutes / 1440);
-    } else if ($distance_in_minutes >= 43200 && $distance_in_minutes <= 86399) {
+    } elseif ($distance_in_minutes >= 43200 && $distance_in_minutes <= 86399) {
         $string = 'about 1 month';
-    } else if ($distance_in_minutes >= 86400 && $distance_in_minutes <= 525959) {
+    } elseif ($distance_in_minutes >= 86400 && $distance_in_minutes <= 525959) {
         $string = '%months% months';
         $parameters['%months%'] = round($distance_in_minutes / 43200);
-    } else if ($distance_in_minutes >= 525960 && $distance_in_minutes <= 1051919) {
+    } elseif ($distance_in_minutes >= 525960 && $distance_in_minutes <= 1051919) {
         $string = 'about 1 year';
     } else {
         $string = 'over %years% years';
