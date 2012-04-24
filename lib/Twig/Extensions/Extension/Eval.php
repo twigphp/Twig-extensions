c4d0f511245e3e9026551bc91f0363fa11e77633
Billy Visto <shibbydude12@hotmail.com>
2012-04-24 13:40:13 -0500

    added extension that adds the eval filter to be used to evaluate twig code that gets passed into a template

---
+++ lib/Twig/Extensions/Extension/Eval.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Twig Extension to add the eval filter to evaluate twig code passed into a template
+ */
+class Twig_Extensions_Extension_Eval extends Twig_Extension
+{
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'eval';
+    }
+
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of filters
+     */
+    public function getFilters()
+    {
+        return array(
+            'eval' => new \Twig_Filter_Method($this, 'evaluateString', array(
+                'needs_environment' => true,
+                'needs_context'     => true,
+            )),
+        );
+    }
+
+    /**
+     * Loads a string template and returns the rendered version
+     *
+     * @param  \Twig_Environment $env
+     * @param  array             $context
+     * @param  string            $string  The string template to load
+     * @return string
+     */
+    public function evaluateString(\Twig_Environment $env, $context, $string)
+    {
+        $newEnv = $this->setLoader($env);
+        return $newEnv->loadTemplate($string)->render($context);
+    }
+
+    /**
+     * Clones the current environment and sets the loader to be a string loader
+     *
+     * @param  \Twig_Environment $env
+     * @return \Twig_Environment
+     */
+    private function setLoader(\Twig_Environment $env)
+    {
+        $newEnv = clone $env;
+        $loader = new \Twig_Loader_String();
+        $newEnv->setLoader($loader);
+        return $newEnv;
+    }
+}
