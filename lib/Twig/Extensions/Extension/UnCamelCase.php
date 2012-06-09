dc2d6024f0de96505f1b8f9b0196df1042e3c672
Billy Visto <bvisto@gustavus.edu>
2012-06-08 22:03:28 -0500

    added filter to convert camel case strings to space separated strings

---
+++ lib/Twig/Extensions/Extension/UnCamelCase.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Twig Extension to add the UnCamelCaseFilter
+ */
+class Twig_Extensions_Extension_UnCamelCase extends Twig_Extension
+{
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'unCamelCase';
+    }
+
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFilters()
+    {
+        return array(
+            'unCamelCase' => new Twig_Filter_Method($this, 'unCamelCaseString'),
+        );
+    }
+
+    /**
+     * Converts a camel case string into a string separated with spaces
+     *
+     * @param  string  $string
+     * @return string
+     */
+    public function unCamelCaseString($string)
+    {
+        $newString = preg_replace('`([A-Z])`', ' $1', $string);
+        return strtolower($newString);
+    }
+}
