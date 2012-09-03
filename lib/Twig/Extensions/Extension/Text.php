22432cf1f64a0de589d01937da25568566fa01e7
darkbluesun <chris@youngfam.co>
2012-09-03 12:44:38 +0100

    fixed syntax for return statement

--- lib/Twig/Extensions/Extension/Text.php
+++ lib/Twig/Extensions/Extension/Text.php
@@ -55,7 +55,8 @@ function twig_linkify_filter($text)
     $text= preg_replace("/(^|[\n ])([\w]*?)((ht|f)tp(s)?:\/\/[\w]+[^ \,\"\n\r\t<]*)/is", "$1$2<a     href=\"$3\" >$3</a>", $text);
     $text= preg_replace("/(^|[\n ])([\w]*?)((www|ftp)\.[^ \,\"\t\n\r<]*)/is", "$1$2<a href=\"http://$3\" >$3</a>", $text);
     $text= preg_replace("/(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+)+)/i", "$1<a href=\"mailto:$2@$3\">$2@$3</a>", $text);
-    return($text);
+    
+    return $text;
 }
 
 if (function_exists('mb_get_info')) {
