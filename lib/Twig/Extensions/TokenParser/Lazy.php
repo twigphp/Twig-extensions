6f64802b97e3f811a386614043e8a20aae4e33cb
xhaleera <christophe@xhaleera.com>
2013-12-16 11:43:51 +0100

    Create Lazy.php

---
+++ lib/Twig/Extensions/TokenParser/Lazy.php
@@ -0,0 +1,35 @@
+class Twig_Extensions_TokenParser_Lazy extends Twig_TokenParser
+{
+    /**
+     * Parses a token and returns a node.
+     *
+     * @param Twig_Token $token A Twig_Token instance
+     *
+     * @return Twig_NodeInterface A Twig_NodeInterface instance
+     */
+    public function parse(Twig_Token $token)
+    {
+        $lineno = $token->getLine();
+			
+			  $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+			  $body = $this->parser->subparse(array($this, 'decideLazyEnd'), true);
+			  $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+			
+			  return new Twig_Extensions_Node_Lazy($body, $lineno, $this->getTag());
+    }
+
+    public function decideLazyEnd(Twig_Token $token)
+    {
+        return $token->test('endlazy');
+    }
+
+    /**
+     * Gets the tag name associated with this token parser.
+     *
+     * @param string The tag name
+     */
+    public function getTag()
+    {
+        return 'lazy';
+    }
+}
