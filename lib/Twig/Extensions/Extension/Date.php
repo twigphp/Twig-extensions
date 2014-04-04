c58b31f3e09548e41db1c954c2d6c703ca939b3b
Andrey Kryukov <a.kryukov.nor@gmail.com>
2014-04-04 12:58:06 +0800

    Add age filter in twig extensions

---
+++ lib/Twig/Extensions/Extension/Date.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file is part of Twig.
+ *
+ * (c) 2009 Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Kryukov <a.kryukov.nor@gmail.com>
+ * @package Twig
+ * @subpackage Twig-extensions
+ */
+class Twig_Extensions_Extension_Date extends Twig_Extension
+{
+    /**
+     * Returns a list of filters.
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        $filters = array(
+             new Twig_SimpleFilter('age', 'twig_age_filter'),
+        );
+
+        return $filters;
+    }
+    /**
+     * Name of this extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'date';
+    }
+}
+
+/**
+ * Returns age depending on birthdate.
+ *
+ * @param DateTime|string  $birthdate
+ * @return integer
+ */
+function twig_age_filter(\DateTime $birthdate)
+{
+    if (!$birthdate instanceof \DateTime) $birthdate = new \DateTime($birthdate);
+    $age = $birthdate->diff(new \DateTime())->y;
+
+    return $age;
+}
---
+++ test/Twig/Tests/Extension/DateTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require_once __DIR__ . '/../../../../lib/Twig/Extensions/Extension/Date.php';
+
+class Twig_Tests_Extension_DateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getAgeFilterTestData
+     */
+    public function testAgeFilter($data, $expected)
+    {
+        $this->assertEquals($expected, twig_age_filter($data));
+    }
+
+
+    public function getAgeFilterTestData()
+    {
+        return array(
+            array(
+                new \DateTime(),
+                0,
+            ),
+            array(
+                '17.01.1980',
+                34,
+            ),
+        );
+    }
+}
