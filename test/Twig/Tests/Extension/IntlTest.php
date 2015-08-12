5021d3951fd46a77c46927a5db201c709d7ed169
rgazelot <r.gazelot@gmail.com>
2015-08-12 14:41:59 +0200

    Add a test for hhvm with authorized timezone

--- test/Twig/Tests/Extension/IntlTest.php
+++ test/Twig/Tests/Extension/IntlTest.php
@@ -43,4 +43,26 @@ class Twig_Tests_Extension_IntlTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('1/1/15 2:00:00 AM GMT+02:00', $date);
     }
+
+    public function testLocalizedDateFilterWithDateTimeZoneOnHHVM()
+    {
+        if (PHP_VERSION_ID < 50500) {
+            $this->markTestSkipped('Only in PHP 5.5+ IntlDateFormatter allows to use DateTimeZone objects.');
+        }
+
+        if (!defined('HHVM_VERSION_ID')) {
+            $this->markTestSkipped('This test is specific for HHVM.');
+        }
+
+        $date = twig_localized_date_filter(
+            $this->env = $this->getMock('Twig_Environment'),
+            new DateTime('2015-01-01T00:00:00', new DateTimeZone('UTC')),
+            'short',
+            'long',
+            'en',
+            'Europe/Paris'
+        );
+
+        $this->assertEquals('1/1/15 1:00:00 AM GMT+01:00', $date);
+    }
 }
