c399513498ff72abca7034eb1dae6360b565748b
Jules Pietri <heah@heahprod.com>
2015-12-04 04:10:06 +0100

    fix fabbot

--- lib/Twig/Extensions/Extension/Link.php
+++ lib/Twig/Extensions/Extension/Link.php
@@ -13,30 +13,30 @@
 class Twig_Extensions_Extension_Link extends Twig_Extension
 {
     /**
-     * List of filters
+     * List of filters.
      *
      * @return array
      */
     public function getFilters()
     {
-        return [
+        return array(
             new \Twig_SimpleFilter('link', array($this, 'createLink'), array(
                 'pre_escape' => 'html',
-                'is_safe' => array('html')
+                'is_safe' => array('html'),
             )),
             new \Twig_SimpleFilter('linkTo', array($this, 'createNamedLink'), array(
                 'pre_escape' => 'html',
-                'is_safe' => array('html')
+                'is_safe' => array('html'),
             )),
             new \Twig_SimpleFilter('mail', array($this, 'createMailTo'), array(
                 'pre_escape' => 'html',
-                'is_safe' => array('html')
+                'is_safe' => array('html'),
             )),
             new \Twig_SimpleFilter('mailTo', array($this, 'createNamedMailTo'), array(
                 'pre_escape' => 'html',
-                'is_safe' => array('html')
-            ))
-        ];
+                'is_safe' => array('html'),
+            )),
+        );
     }
 
     /**
@@ -52,9 +52,9 @@ class Twig_Extensions_Extension_Link extends Twig_Extension
     /**
      * Create an html link from an url.
      *
-     * @param string  $url        input
-     * @param array   $attributes html attributes e.g class, target, id
-     * @param boolean $withScheme let the scheme explicit or not
+     * @param string $url        input
+     * @param array  $attributes html attributes e.g class, target, id
+     * @param bool   $withScheme let the scheme explicit or not
      *
      * @return string html formatted link
      *
@@ -66,20 +66,20 @@ class Twig_Extensions_Extension_Link extends Twig_Extension
             throw new \Twig_Error_Runtime('Invalid argument passed to "link" filter, must be a string.');
         }
 
-        $url = 'http' === substr($url, 0, 4) ? $url : 'http://' . $url;
+        $url = 'http' === substr($url, 0, 4) ? $url : 'http://'.$url;
         $parts = explode('://', $url);
         $link = $withScheme ? $url : $parts[1];
         $attr = '';
 
         foreach ($attributes as $attribute => $value) {
-            $attr .= ' ' . $attribute . '="' . $value . '"';
+            $attr .= ' '.$attribute.'="'.$value.'"';
         }
 
-        return '<a href="' . urlencode($url) . '"' . $attr . '>' . $link . '</a>';
+        return '<a href="'.urlencode($url).'"'.$attr.'>'.$link.'</a>';
     }
 
     /**
-     * Create a named html link from an url
+     * Create a named html link from an url.
      *
      * @param string $url        input
      * @param string $link       output name of the link
@@ -95,14 +95,14 @@ class Twig_Extensions_Extension_Link extends Twig_Extension
             throw new \Twig_Error_Runtime('Invalid argument passed to "link" filter, must be a string.');
         }
 
-        $url = 'http' === substr($url, 0, 4) ? $url : 'http://' . $url;
+        $url = 'http' === substr($url, 0, 4) ? $url : 'http://'.$url;
         $attr = '';
 
         foreach ($attributes as $attribute => $value) {
-            $attr .= ' ' . $attribute . '="' . $value . '"';
+            $attr .= ' '.$attribute.'="'.$value.'"';
         }
 
-        return '<a href="' . urlencode($url) . '"' . $attr . '>' . $link . '</a>';
+        return '<a href="'.urlencode($url).'"'.$attr.'>'.$link.'</a>';
     }
 
     /**
@@ -128,26 +128,26 @@ class Twig_Extensions_Extension_Link extends Twig_Extension
         $params = array();
 
         if ($subject) {
-            $params[] = 'subject=' . rawurlencode($subject);
+            $params[] = 'subject='.rawurlencode($subject);
         }
 
         if ($body) {
-            $params[] =  'body=' . rawurlencode($body);
+            $params[] =  'body='.rawurlencode($body);
         }
 
         if ($params) {
-            $extra .= '?' . implode('&', $params);
+            $extra .= '?'.implode('&', $params);
         }
 
         foreach ($attributes as $attribute => $value) {
-            $attr .= ' ' . $attribute . '="' . $value . '"';
+            $attr .= ' '.$attribute.'="'.$value.'"';
         }
 
-        return '<a href="mailto:' . $mail . $extra . '"' . $attr . '>' . $mail . '</a>';
+        return '<a href="mailto:'.$mail.$extra.'"'.$attr.'>'.$mail.'</a>';
     }
 
     /**
-     * Create a named mailto link from an e-mail
+     * Create a named mailto link from an e-mail.
      *
      * @param string $mail       input e-mail
      * @param string $link       input name of the link
@@ -170,21 +170,21 @@ class Twig_Extensions_Extension_Link extends Twig_Extension
         $params = array();
 
         if ($subject) {
-            $params[] = 'subject=' . rawurlencode($subject);
+            $params[] = 'subject='.rawurlencode($subject);
         }
 
         if ($body) {
-            $params[] =  'body=' . rawurlencode($body);
+            $params[] =  'body='.rawurlencode($body);
         }
 
         if ($params) {
-            $extra .= '?' . implode('&', $params);
+            $extra .= '?'.implode('&', $params);
         }
 
         foreach ($attributes as $attribute => $value) {
-            $attr .= ' ' . $attribute . '="' . $value . '"';
+            $attr .= ' '.$attribute.'="'.$value.'"';
         }
 
-        return '<a href="mailto:' . $mail . $extra . '"' . $attr . '>' . $link . '</a>';
+        return '<a href="mailto:'.$mail.$extra.'"'.$attr.'>'.$link.'</a>';
     }
 }
--- test/Twig/Tests/Extension/LinkTest.php
+++ test/Twig/Tests/Extension/LinkTest.php
@@ -78,32 +78,32 @@ class Twig_Tests_Extension_LinkTest extends PHPUnit_Framework_TestCase
         return array(
             array(
                 'twig.sensiolabs.org/documentation', array(), false,
-                '<a href="http://twig.sensiolabs.org/documentation">twig.sensiolabs.org/documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation">twig.sensiolabs.org/documentation</a>',
             ),
             array(
                 'https://github.com/twigphp/Twig', array(), false,
-                '<a href="https://github.com/twigphp/Twig">github.com/twigphp/Twig</a>'
+                '<a href="https://github.com/twigphp/Twig">github.com/twigphp/Twig</a>',
             ),
             array(
                 'twig.sensiolabs.org/documentation', array(), true,
-                '<a href="http://twig.sensiolabs.org/documentation">http://twig.sensiolabs.org/documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation">http://twig.sensiolabs.org/documentation</a>',
             ),
             array(
                 'https://github.com/twigphp/Twig', array(), true,
-                '<a href="https://github.com/twigphp/Twig">https://github.com/twigphp/Twig</a>'
+                '<a href="https://github.com/twigphp/Twig">https://github.com/twigphp/Twig</a>',
             ),
             array(
                 'twig.sensiolabs.org/documentation', array('class' => 'mylinks', 'target' => '_blank'), false,
-                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">twig.sensiolabs.org/documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">twig.sensiolabs.org/documentation</a>',
             ),
             array('https://github.com/twigphp/Twig', array('class' => 'mylinks', 'target' => '_blank'), false,
-                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">github.com/twigphp/Twig</a>'
+                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">github.com/twigphp/Twig</a>',
             ),
             array('twig.sensiolabs.org/documentation', array('class' => 'mylinks', 'target' => '_blank'), true,
-                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">http://twig.sensiolabs.org/documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">http://twig.sensiolabs.org/documentation</a>',
             ),
             array('https://github.com/twigphp/Twig', array('class' => 'mylinks', 'target' => '_blank'), true,
-                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">https://github.com/twigphp/Twig</a>'
+                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">https://github.com/twigphp/Twig</a>',
             )
         );
     }
@@ -112,16 +112,16 @@ class Twig_Tests_Extension_LinkTest extends PHPUnit_Framework_TestCase
     {
         return array(
             array('twig.sensiolabs.org/documentation', 'Twig Documentation', array(),
-                '<a href="http://twig.sensiolabs.org/documentation">Twig Documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation">Twig Documentation</a>',
             ),
             array('https://github.com/twigphp/Twig', 'Twig on GitHub', array(),
-                '<a href="https://github.com/twigphp/Twig">Twig on GitHub</a>'
+                '<a href="https://github.com/twigphp/Twig">Twig on GitHub</a>',
             ),
             array('twig.sensiolabs.org/documentation', 'Twig Documentation', array('class' => 'mylinks', 'target' => '_blank'),
-                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">Twig Documentation</a>'
+                '<a href="http://twig.sensiolabs.org/documentation" class="mylinks" target="_blank">Twig Documentation</a>',
             ),
             array('https://github.com/twigphp/Twig', 'Twig on GitHub', array('class' => 'mylinks', 'target' => '_blank'),
-                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">Twig on GitHub</a>'
+                '<a href="https://github.com/twigphp/Twig" class="mylinks" target="_blank">Twig on GitHub</a>',
             )
         );
     }
@@ -131,23 +131,23 @@ class Twig_Tests_Extension_LinkTest extends PHPUnit_Framework_TestCase
         return array(
             array(
                 'fabien@symfony.com', array(), '', '',
-                '<a href="mailto:fabien@symfony.com">fabien@symfony.com</a>'
+                '<a href="mailto:fabien@symfony.com">fabien@symfony.com</a>',
             ),
             array(
                 'fabien@symfony.com', array('class' => 'mail-links'), '', '',
-                '<a href="mailto:fabien@symfony.com" class="mail-links">fabien@symfony.com</a>'
+                '<a href="mailto:fabien@symfony.com" class="mail-links">fabien@symfony.com</a>',
             ),
             array(
                 'fabien@symfony.com', array(), 'What about Twig ?', '',
-                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F">fabien@symfony.com</a>'
+                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F">fabien@symfony.com</a>',
             ),
             array(
                 'fabien@symfony.com', array(), '', 'Thank you for bringing PHP to a higher level for 10 years',
-                '<a href="mailto:fabien@symfony.com?body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years">fabien@symfony.com</a>'
+                '<a href="mailto:fabien@symfony.com?body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years">fabien@symfony.com</a>',
             ),
             array(
                 'fabien@symfony.com', array('class' => 'mail-links'), 'What about Twig ?', 'Thank you for bringing PHP to a higher level for 10 years',
-                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F&body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years" class="mail-links">fabien@symfony.com</a>'
+                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F&body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years" class="mail-links">fabien@symfony.com</a>',
             ),
         );
     }
@@ -157,23 +157,23 @@ class Twig_Tests_Extension_LinkTest extends PHPUnit_Framework_TestCase
         return array(
             array(
                 'fabien@symfony.com', 'Fabien', array(), '', '',
-                '<a href="mailto:fabien@symfony.com">Fabien</a>'
+                '<a href="mailto:fabien@symfony.com">Fabien</a>',
             ),
             array(
                 'fabien@symfony.com', 'Fabien', array('class' => 'mail-links'), '', '',
-                '<a href="mailto:fabien@symfony.com" class="mail-links">Fabien</a>'
+                '<a href="mailto:fabien@symfony.com" class="mail-links">Fabien</a>',
             ),
             array(
                 'fabien@symfony.com', 'Fabien', array(), 'What about Twig ?', '',
-                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F">Fabien</a>'
+                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F">Fabien</a>',
             ),
             array(
                 'fabien@symfony.com', 'Fabien', array(), '', 'Thank you for bringing PHP to a higher level for 10 years',
-                '<a href="mailto:fabien@symfony.com?body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years">Fabien</a>'
+                '<a href="mailto:fabien@symfony.com?body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years">Fabien</a>',
             ),
             array(
                 'fabien@symfony.com', 'Fabien', array('class' => 'mail-links'), 'What about Twig ?', 'Thank you for bringing PHP to a higher level for 10 years',
-                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F&body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years" class="mail-links">Fabien</a>'
+                '<a href="mailto:fabien@symfony.com?subject=What%20about%20Twig%20%3F&body=Thank%20you%20for%20bringing%20PHP%20to%20a%20higher%20level%20for%2010%20years" class="mail-links">Fabien</a>',
             ),
         );
     }
