4ddc694b7b179218dd533ca073e327fc09570ec8
Erik C. Olson <erikcaineolson@gmail.com>
2016-04-29 13:26:03 -0700

    Determines whether a route exists
    Provides a safety check for routes, avoiding Twig errors when they don't exist (recommend against using it in development, where you should know anytime a route doesn't exist).

---
+++ lib/Twig/Extensions/Extension/RouteExists.php
@@ -0,0 +1,34 @@
+<?php namespace AppBundle\Twig;
+
+use Symfony\Component\DependencyInjection\Container;
+use \Twig_Extension;
+use \Twig_SimpleFilter;
+
+class RouteExistsExtension extends Twig_Extension
+{
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new Twig_SimpleFilter('routeExists', [$this, 'routeExistsFilter']),
+        ];
+    }
+
+    public function routeExistsFilter($route)
+    {
+        $router = $this->container->get('router');
+
+        return (null === $router->getRouteCollection()->get($route)) ? false : true;
+    }
+
+    public function getName()
+    {
+        return 'app_route_exists_extension';
+    }
+}
