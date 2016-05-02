3dbe001350c74822a6740e1f0aebba9a06497720
Erik C. Olson <erikcaineolson@gmail.com>
2016-05-02 11:28:56 -0700

    Create RouteExistsExtensionTest.php
    Implement test for RouteExistsExtension.

---
+++ test/Twig/Tests/Extension/RouteExistsExtensionTest.php
@@ -0,0 +1,71 @@
+<?php namespace AppBundle\Tests\Twig\Extension;
+
+use AppBundle\Twig\RouteExistsExtension;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RouteExistsExtensionTest extends WebTestCase
+{
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Client
+     */
+    protected $client;
+
+    /**
+     * @var \AppBundle\Twig\RouteExistsExtension
+     */
+    protected $routeExistsExtension;
+
+    /**
+     * RouteExistsExtensionTest constructor
+     * 
+     * Initialize a client so we can inject the container into the RouteExistsExtension
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->client = static::createClient();
+        
+        $this->routeExistsExtension = new RouteExistsExtension($this->client->getContainer());
+    }
+
+    /**
+     * Get sample data
+     *
+     * @return array
+     */
+    public function getRouteExistsExtensionTestData()
+    {
+        return [
+            '_profiler_home'            => true,        // should always exist (if not, change to one that does)
+            'this_route_does_not_exist' => false,       // should never exist (if you use this route, change the value)
+        ];
+    }
+
+    /**
+     * Filter test
+     *
+     * @param array|null $data
+     * @param array|null $expectedResults
+     */
+    public function testRouteExistsExtension(array $data = null, array $expectedResults = null)
+    {
+        $dataCount = count($data);
+
+        for ($i = 0; $i < $dataCount; $i++) {
+            if($expectedResults[$i] === false){
+                $this->assertFalse($this->routeExistsExtension->routeExistsFilter($data[$i]));
+            }elseif ($expectedResults[$i] === true){
+                $this->assertTrue($this->routeExistsExtension->routeExistsFilter($data[$i]));
+            }
+        }
+    }
+
+    /**
+     * An empty string should result in FALSE
+     */
+    public function testRouteExistsExtensionOnEmptyString()
+    {
+        $this->assertFalse($this->routeExistsExtension->routeExistsFilter(''));
+    }
+}
