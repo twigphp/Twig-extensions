f2e00ba9c27e4013853841aa95b0d7a8552b8e09
Erik C. Olson <erikcaineolson@gmail.com>
2016-05-02 14:16:33 -0700

    Adds Format
    Allows user to select between SI and IEC formats. Defaults to IEC for generally expected calculations.

--- lib/Twig/Extensions/Extension/HumanReadableBytes.php
+++ lib/Twig/Extensions/Extension/HumanReadableBytes.php
@@ -1,11 +1,23 @@
-<?php
+<?php namespace AppBundle\Twig\Extension;
 
-class HumanReadableBytes extends \Twig_Extension
+use \Twig_Error;
+use \Twig_Extension;
+use \Twig_SimpleFilter;
+
+class HumanReadableBytes extends Twig_Extension
 {
-    const KILOBYTE = 1024;
-    const MEGABYTE = 1024 * self::KILOBYTE;
-    const GIGABYTE = 1024 * self::MEGABYTE;
-    const TERABYTE = 1024 * self::GIGABYTE;
+    const KILOBYTE = 1000;
+    const MEGABYTE = 1000 * self::KILOBYTE;
+    const GIGABYTE = 1000 * self::MEGABYTE;
+    const TERABYTE = 1000 * self::GIGABYTE;
+
+    const KIBIBYTE = 1024;
+    const MEBIBYTE = 1024 * self::KIBIBYTE;
+    const GIBIBYTE = 1024 * self::MEBIBYTE;
+    const TEBIBYTE = 1024 * self::GIBIBYTE;
+
+    const SUFFIX_SI = 'B';
+    const SUFFIX_IEC = 'iB';
 
     /**
      * {@inheritdoc}
@@ -13,7 +25,7 @@ class HumanReadableBytes extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('readBytes', [
+            new Twig_SimpleFilter('readBytes', [
                 $this,
                 'humanReadableBytesFilter'
             ]),
@@ -31,22 +43,48 @@ class HumanReadableBytes extends \Twig_Extension
      * @return string
      * @throws \Twig_Error
      */
-    public function humanReadableBytesFilter($bytes, $decimalPlaces = 2, $decimalPoint = '.', $thousandsSeparator = ',')
+    public function humanReadableBytesFilter($bytes, $decimalPlaces = 2, $decimalPoint = '.', $thousandsSeparator = ',', $format = 'IEC')
     {
         if (!is_numeric($bytes)) {
-            throw new \Twig_Error('Data must be numeric');
+            throw new Twig_Error('Data must be numeric');
         }
 
-        if ($bytes < self::KILOBYTE) {
+        $multipliers = [];
+
+        switch ($format) {
+            case 'SI':
+                $multipliers = [
+                    'K' => self::KILOBYTE,
+                    'M' => self::MEGABYTE,
+                    'G' => self::GIGABYTE,
+                    'T' => self::TERABYTE,
+                ];
+
+                $suffix = self::SUFFIX_SI;
+                break;
+            case 'IEC':
+            default:
+                $multipliers = [
+                    'K' => self::KIBIBYTE,
+                    'M' => self::MEBIBYTE,
+                    'G' => self::GIBIBYTE,
+                    'T' => self::TEBIBYTE,
+                ];
+
+                $suffix = self::SUFFIX_IEC;
+                break;
+        }
+
+        if ($bytes < $multipliers['K']) {
             $readable = number_format($bytes, $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' B';
-        } elseif ($bytes < self::MEGABYTE) {
-            $readable = number_format($bytes / self::KILOBYTE, $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' KB';
-        } elseif ($bytes < self::GIGABYTE) {
-            $readable = number_format($bytes / self::MEGABYTE, $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' MB';
-        } elseif ($bytes < self::TERABYTE) {
-            $readable = number_format($bytes / self::GIGABYTE, $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' GB';
+        } elseif ($bytes < $multipliers['M']) {
+            $readable = number_format($bytes / $multipliers['K'], $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' K' . $suffix;
+        } elseif ($bytes < $multipliers['G']) {
+            $readable = number_format($bytes / $multipliers['M'], $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' M' . $suffix;
+        } elseif ($bytes < $multipliers['T']) {
+            $readable = number_format($bytes / $multipliers['G'], $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' G' . $suffix;
         } else {
-            $readable = number_format($bytes / self::TERABYTE, $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' TB';
+            $readable = number_format($bytes / $multipliers['T'], $decimalPlaces, $decimalPoint, $thousandsSeparator) . ' T' . $suffix;
         }
 
         return $readable;
