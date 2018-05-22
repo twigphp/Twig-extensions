c6b43095c0f7ad5d76207c085793496d59d0b64a
Edi Modrić <edi.modric@gmail.com>
2018-05-22 14:30:07 +0200

    Add package_version and pretty_package_version Twig functions

---
+++ src/VersionExtension.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Twig\Extensions;
+
+class_exists('Twig_Extensions_Extension_Version');
+
+if (\false) {
+    class VersionExtension extends \Twig_Extensions_Extension_Version
+    {
+    }
+}
